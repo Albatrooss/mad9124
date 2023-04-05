0e839be4f1ae962bf3d27b1a3dd7c73c7aaf6088
Tim Robillard <robillt@algonquincollege.com>
2023-04-05 18:57:02 -0400

    remove testApp

--- week13/testApp/src/utils/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const createLogger = require("./logger");
-const logger = createLogger("db");
-
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  logger.info(`connected to mongo at ${process.env.MONGO_URL}`);
-});
