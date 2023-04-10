9c8e70d2efad2bfaf4600577b57a2d59db81f40a
Tim Robillard <robillt@algonquincollege.com>
2023-04-10 19:14:39 -0400

    add dynamic redirects

--- week14/pokemonApi/src/router/auth.js
+++ week14/pokemonApi/src/router/auth.js
@@ -9,16 +9,27 @@ require("../utils/passport");
 const authRouter = Router();
 
 // this redirects to google
-authRouter.get(
-  "/google",
-  passport.authenticate("google", { scope: ["profile"] })
-);
+authRouter.get("/google", (req, res, next) => {
+  //localhost:3001/auth/google?redirect_url=http://localhost:3000/login-success
+  const { redirect_url } = req.query;
+
+  const authenticator = passport.authenticate("google", {
+    scope: ["profile"],
+    state: redirect_url,
+  });
+  return authenticator(req, res, next);
+});
 
 // google sends response to this
 authRouter.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
+    // lookup the state from googl
+    const { state } = req.query;
+    // define redirectUrl from the state or default to '/api/pokemon'
+    const redirectUrl = state ?? '/api/pokemon';
+
     // get the user's id
     const id = req.user._id.toString();
 
@@ -26,7 +37,7 @@ authRouter.get(
     const token = jwt.sign({ id }, process.env.JWT_SECRET);
 
     //redirect with the token
-    res.redirect(`/api/pokemon?token=${token}`);
+    res.redirect(`${redirectUrl}?token=${token}`);
   }
 );
 
