50b81160193bd7fe37c44befce1d96909d0c64ea
Tim Robillard <robillt@algonquincollege.com>
2023-04-10 19:47:08 -0400

    add timestamps

--- week14/pokemonApi/src/models/pokemon.js
+++ week14/pokemonApi/src/models/pokemon.js
@@ -1,19 +1,24 @@
 const { Schema, model } = require("mongoose");
 
-const pokemonSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  abilities: [
-    {
+const pokemonSchema = new Schema(
+  {
+    name: {
       type: String,
+      required: true,
     },
-  ],
-});
+    type: {
+      type: String,
+      required: true,
+    },
+    abilities: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = model("pokemon", pokemonSchema);
