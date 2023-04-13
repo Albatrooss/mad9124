333bcdb04fde0e86cf89ed2ba04e21fb33b3745f
Tim Robillard <robillt@algonquincollege.com>
2023-04-12 20:44:45 -0400

    redirect_url

--- week14/pokemonApi/src/router/auth.js
+++ week14/pokemonApi/src/router/auth.js
@@ -9,24 +9,39 @@ require("../utils/passport");
 const authRouter = Router();
 
 // this redirects to google
-authRouter.get(
-  "/google",
-  passport.authenticate("google", { scope: ["profile"] })
-);
+authRouter.get("/google", (req, res, next) => {
+  // get the redirect_url from the query params
+  // expecting the url to look like /auth/google?redirect_url=http://localhost:3000/
+  const { redirect_url } = req.query;
+
+  // add it to the google state
+  const authenticator = passport.authenticate("google", {
+    scope: ["profile"],
+    state: redirect_url,
+  });
+
+  // redirect to google.
+  authenticator(req, res, next);
+});
 
 // google sends response to this
 authRouter.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
+    // get the state from the query params
+    const { state } = req.query;
+
+    // define the redirectUrl with a fallback if undefined
+    const redirectUrl = state ?? "/api/pokemon";
+
     // get the user id
     const id = req.user._id.toString();
-
     // generate the token
     const token = jwt.sign({ id }, process.env.JWT_SECRET);
 
     // redirect with the token
-    res.redirect(`/api/pokemon?token=${token}`);
+    res.redirect(`${redirectUrl}?token=${token}`);
   }
 );
 
